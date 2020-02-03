7101fb0672d3d9b15da01ff58f07487d0f9f9be8
Antonio Tenllado <atenllado@iciredimpagados.com>
2020-02-03 06:03:17 +0000

    Finish user edit, update, index, and destroy actions

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -1,15 +1,38 @@
-require 'test_helper'
-class SessionsHelperTest < ActionView::TestCase
-def setup
-@user = users(:michael)
-remember(@user)
-end
-test "current_user returns right user when session is nil" do
-assert_equal @user, current_user
-assert is_logged_in?
-end
-test "current_user returns nil when remember digest is wrong" do
-@user.update_attribute(:remember_digest, User.digest(User.new_token))
-assert_nil current_user
-end
+module SessionsHelper
+# Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+# Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+# Returns the user corresponding to the remember token cookie.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+       @current_user = user
+      end
+    end
+  end
+# Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+# Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+# Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
